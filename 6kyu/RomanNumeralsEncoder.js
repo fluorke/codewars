f4ac94d44691111be914b0aea7b09a3a188a46e1
fluorke <fluoropla@gmail.com>
2022-06-20 22:17:50 +0300

    Roman Numerals Encoder

--- 6kyu/RomanNumeralsEncoder.js
+++ 6kyu/RomanNumeralsEncoder.js
@@ -9,9 +9,7 @@ const rome = {
 };
 
 const range = (x) => {
-  //console.log(`in range ==== x is ${x}`);
   if (x == 0) return 0;
- //let r = [1, 5, 10];
   if(x >= 5) {
     if(x > 8) return 10;
     return 5;
@@ -20,19 +18,15 @@ const range = (x) => {
   } else return 1;
 }
 
-let number = 4;
 
-// [3, 7, 2, 0]
-let n = (number).toString().split('').map(n => parseInt(n)).reverse();
-//console.log(`y is ${n}`)
-//let n = [7,0,0,2];
-
-console.log(n);
-let x = 0, min = 1, str = "", mid = 5, max = 10;
-let minS = "I", midS = "V", maxS = "X";
-let count = 1;
-
-for (let i = 0; i < n.length; i++) {
+function solution(number){
+  let x = 0, min = 1, str = "", mid = 5, max = 10;
+  let minS = "I", midS = "V", maxS = "X";
+  let count = 1;
+  
+  let n = (number).toString().split('').map(n => parseInt(n)).reverse();
+  
+  for (let i = 0; i < n.length; i++) {
   if(i == 3) x = 1;
   else x = range(n[i]);
   let tempStr = "";
@@ -61,33 +55,25 @@ for (let i = 0; i < n.length; i++) {
 
   switch(x) {
     case mid:
-    //str+=midS;
     tempStr+=midS;
     break;
     case min:
-    //str+=minS;
     tempStr+=minS;
     break;
     case max:
-    //str+=maxS;
     tempStr+=maxS;
     break;
     default: 0;
   }
 
-  console.log(`x before while ${x}`)
-
-  //console.log('before while', n[i], 'x is ' + x);
   let count = 1;
   while(n[i] != x && n[i] != 0) {
-    //console.log(`while ==== temp is ${tempStr}, str is ${str}, x is ${x}`)
+    console.log(`${tempStr}`)
 
     if(minS != "M") {
       if(x > n[i]) {
-        //str = minS + str;
-        tempStr += minS;
         x--;
-        console.log(`======= x is ${x}, tempStr is ${tempStr}`)
+        tempStr += minS;
       } else {
         tempStr = minS + tempStr;
         x++;
@@ -102,15 +88,10 @@ for (let i = 0; i < n.length; i++) {
         x++;
         count++;
       }
-      //console.log(`temp str is ${tempStr}`)
       tempStr = tempStr.split('').reverse().join('');
     }
 
-    //console.log(`x is ${x}, count is ${count} tempStr ${tempStr}`);
-    //str += minS;
   }
-
-  //tempStr.split('').reverse().join('');
   if(n[i] != 0) {
     str += tempStr;
   } else if (n[3] == 0) {
@@ -118,8 +99,6 @@ for (let i = 0; i < n.length; i++) {
   }
 }
   
-console.log(n, "\n");
-console.log(str.split('').reverse().join(''));
-//console.log(str);
-
+  return str.split('').reverse().join('');
+}
 
